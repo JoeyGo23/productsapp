891612cce7f418289b12d73cb5fb95a6373b7594
Joe G <wheeltirefamily@gmail.com>
2014-02-05 17:35:12 -0600

    rails new productsapp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Productsapp::Application.config.secret_key_base = 'f11531ae37f50bd674a192e8633a3729b9376dce6c8640d9bbee32cad73b12ff261d50bf19827bf2e5eaf1608cb41bd9871ba9a5dc4f03006605048981225132'
