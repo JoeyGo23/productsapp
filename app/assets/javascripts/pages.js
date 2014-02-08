0e3e791ef3ac26460a1cc5cce363ea1ccebdc2b0
Joe G <wheeltirefamily@gmail.com>
2014-02-08 15:50:53 -0600

    drag clone helper work, but not when drop it sticks to droppable.

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
 	console.log("Jscript Lima Charlie");
-	
+	 $( ".product-container" ).draggable({
+        helper: "clone"
+    });
+
+    $( ".cart-droppable-area" ).droppable({
+        activeClass: "droppable",
+        hoverClass: "hover",
+        tolerance: "touch",
+        drop: function( event, ui ) {
+            var productObj = $(this).find('div');
+            listObj.append(ui.draggable.clone());
+        }
+    });
+
 });
